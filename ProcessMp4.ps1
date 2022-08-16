f711a4812f8d6ab7ae05d03fdc2f7d1c00c74670
Federico Daniel Colombo <thepirat000@hotmail.com>
2022-08-15 22:23:38 -0500

    Update ProcessMp4.ps1

--- ProcessMp4.ps1
+++ ProcessMp4.ps1
@@ -6,15 +6,15 @@ for ($i=0; $i -lt $files.Count; $i++) {
 
     $perc = [math]::Round($i * 100 / $files.Count, 0)
     Write-Progress -Activity "Conversion in progress" -Status "$perc% Complete:" -PercentComplete $perc
-	$outFile = "p\$name"
+    $outFile = "p\$name"
 
-	# Process video
+    # Process video
     & ffmpeg -i $f.FullName -c:v libx265 -b:v 3500k $outFile
 	
-	# Copy metadata
+    # Copy metadata
     & exiftool -overwrite_Original -TagsFromFile $f.FullName -All:All $outFile
 
-	# Copy file dates
+    # Copy file dates
     (ls $outfile).CreationTime = (ls $f.FullName).CreationTime
     (ls $outfile).LastWriteTime = (ls $f.FullName).LastWriteTime
     (ls $outfile).LastAccessTime = (ls $f.FullName).LastAccessTime
